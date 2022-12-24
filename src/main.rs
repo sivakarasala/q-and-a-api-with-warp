45c4a6b4f30904b71246d61cd8689310af4909d2
SivaKrishna <shivataraya@gmail.com>
2022-12-24 20:00:47 +0530

    add handler for questions and setup cors

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use std::str::FromStr;
 use std::io::{Error, ErrorKind};
+use serde::Serialize;
 
-use warp::Filter;
+use warp::{
+    Filter,
+    http::Method, 
+    filters::{
+        cors::CorsForbidden,
+    }, 
+    reject::Reject, 
+    Rejection, 
+    Reply, 
+    http::StatusCode
+};
 
 #[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -11,7 +26,7 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
 impl Question {
@@ -36,12 +51,61 @@ impl FromStr for QuestionId {
     }
 }
 
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!("faq".to_string())),
+    );
+
+    match question.id.0.parse::<i32>() {
+        Err(_) => {
+            Err(warp::reject::custom(InvalidId))
+        },
+        Ok(_) => {
+            Ok(warp::reply::json(
+                &question
+            ))
+        }
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(error) = r.find::<CorsForbidden>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(InvalidId) = r.find::<InvalidId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found".to_string(),
+            StatusCode::NOT_FOUND
+        ))
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let hello = warp::get()
-        .map(|| format!("Hello, world!"));
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
 
-    warp::serve(hello)
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
+    
+    let routes = get_items.with(cors);
+
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
