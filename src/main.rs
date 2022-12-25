f2b975f77b50b06fdcb506c7358b494897ed8bca
SivaKrishna <shivataraya@gmail.com>
2022-12-25 21:06:32 +0530

    add delete question handler

--- src/main.rs
+++ src/main.rs
@@ -95,6 +95,23 @@ async fn update_question(
     ))
 }
 
+async fn delete_question(
+    id: String,
+    store: Store,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    match store.questions.write().await.remove(&QuestionId(id)) {
+        Some(_) => {
+            return Ok(
+                warp::reply::with_status(
+                    "Question deleted",
+                    StatusCode::OK
+                )
+            )
+        },
+        None => return Err(warp::reject::custom(Error::QuestionNotFound)),
+    }
+}
+
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     if let Some(error) = r.find::<Error>() {
         Ok(warp::reply::with_status(
@@ -194,10 +211,18 @@ async fn main() {
         .and(store_filter.clone())
         .and(warp::body::json())
         .and_then(update_question);
+
+    let delete_question = warp::delete()
+        .and(warp::path("questions"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and_then(delete_question);
     
     let routes = get_questions
-        .or(add_question)
         .or(update_question)
+        .or(add_question)
+        .or(delete_question)
         .with(cors)
         .recover(return_error);
 
