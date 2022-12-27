a1007c31c01f7b7c5200d8a323cbb2c0ac08fb73
SivaKrishna <shivataraya@gmail.com>
2022-12-27 16:08:12 +0530

    add db error & tracing support

--- handle-errors/src/lib.rs
+++ handle-errors/src/lib.rs
@@ -1,54 +1,58 @@
-use warp::{ 
-    filters::{
-        body::BodyDeserializeError,
-        cors::CorsForbidden,
-    }, 
-    reject::Reject, 
-    Rejection, 
-    Reply, 
-    http::StatusCode
+use warp::{
+    filters::{body::BodyDeserializeError, cors::CorsForbidden},
+    http::StatusCode,
+    reject::Reject,
+    Rejection, Reply,
 };
+use tracing::{event, Level, instrument};
 
+#[instrument]
 pub async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-  if let Some(error) = r.find::<Error>() {
-      Ok(warp::reply::with_status(
-          error.to_string(), 
-          StatusCode::RANGE_NOT_SATISFIABLE,))
-  } else if let Some(error) = r.find::<CorsForbidden>() {
-      Ok(warp::reply::with_status(
-          error.to_string(),
-          StatusCode::FORBIDDEN,
-      ))
-  } else if let Some(error) = r.find::<BodyDeserializeError>() {
-      Ok(warp::reply::with_status(
-          error.to_string(),
-          StatusCode::UNPROCESSABLE_ENTITY,
-      ))
-  } else {
-      Ok(warp::reply::with_status(
-          "Route not found".to_string(),
-          StatusCode::NOT_FOUND
-      ))
-  }
+    if let Some(crate::Error::DatabaseQueryError) = r.find() {
+        event!(Level::ERROR, "Database query error");
+        Ok(warp::reply::with_status(crate::Error::DatabaseQueryError.to_string(), StatusCode::UNPROCESSABLE_ENTITY))
+    } else if let Some(error) = r.find::<CorsForbidden>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(error) = r.find::<BodyDeserializeError>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else if let Some(error) = r.find::<Error>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::RANGE_NOT_SATISFIABLE,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found".to_string(),
+            StatusCode::NOT_FOUND,
+        ))
+    }
 }
 
 #[derive(Debug)]
 pub enum Error {
-  ParseError(std::num::ParseIntError),
-  MissingParameters,
-  QuestionNotFound,
+    ParseError(std::num::ParseIntError),
+    MissingParameters,
+    DatabaseQueryError,
 }
 
 impl std::fmt::Display for Error {
-  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-      match *self {
-          Error::ParseError(ref err) => {
-              write!(f, "Cannot parse parameter: {}", err)
-          },
-          Error::MissingParameters => write!(f, "Missing parameter"),
-          Error::QuestionNotFound => write!(f, "Question not found"),
-      }
-  }
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Error::ParseError(ref err) => {
+                write!(f, "Cannot parse parameter: {}", err)
+            }
+            Error::MissingParameters => write!(f, "Missing parameter"),
+            Error::DatabaseQueryError => {
+                write!(f, "Cannot update, invalid data.")
+            }
+        }
+    }
 }
 
 impl Reject for Error {}
