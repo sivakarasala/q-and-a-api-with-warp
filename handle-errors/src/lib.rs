a1537cf38bc5594237c7904b181fbf81050373cc
SivaKrishna <shivataraya@gmail.com>
2022-12-28 17:06:58 +0530

    add external api errors

--- handle-errors/src/lib.rs
+++ handle-errors/src/lib.rs
@@ -5,12 +5,28 @@ use warp::{
     Rejection, Reply,
 };
 use tracing::{event, Level, instrument};
+use reqwest::Error as ReqwestError;
 
 #[instrument]
 pub async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     if let Some(crate::Error::DatabaseQueryError) = r.find() {
         event!(Level::ERROR, "Database query error");
         Ok(warp::reply::with_status(crate::Error::DatabaseQueryError.to_string(), StatusCode::UNPROCESSABLE_ENTITY))
+    } else if let Some(crate::Error::ExternalAPIError(e)) = r.find() {
+        event!(Level::ERROR, "{}", e);
+        Ok(warp::reply::with_status("Internal Server Error".to_string(), StatusCode::INTERNAL_SERVER_ERROR))
+    } else if let Some(crate::Error::ClientError(e)) = r.find() {
+        event!(Level::ERROR, "{}", e);
+        Ok(warp::reply::with_status(
+            "Internal Server Error".to_string(),
+            StatusCode::INTERNAL_SERVER_ERROR,
+        ))
+    } else if let Some(crate::Error::ServerError(e)) = r.find() {
+        event!(Level::ERROR, "{}", e);
+        Ok(warp::reply::with_status(
+            "Internal Server Error".to_string(), 
+            StatusCode::INTERNAL_SERVER_ERROR,
+        ))
     } else if let Some(error) = r.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
             error.to_string(),
@@ -39,20 +55,45 @@ pub enum Error {
     ParseError(std::num::ParseIntError),
     MissingParameters,
     DatabaseQueryError,
+    ExternalAPIError(ReqwestError),
+    ClientError(APILayerError),
+    ServerError(APILayerError),
+}
+
+#[derive(Debug, Clone)]
+pub struct APILayerError {
+    pub status: u16,
+    pub message: String,
+}
+
+impl std::fmt::Display for APILayerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Status: {}, Message: {}", self.status, self.message)
+    }
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match *self {
-            Error::ParseError(ref err) => {
+        match &*self {
+            Error::ParseError(err) => {
                 write!(f, "Cannot parse parameter: {}", err)
             }
             Error::MissingParameters => write!(f, "Missing parameter"),
             Error::DatabaseQueryError => {
                 write!(f, "Cannot update, invalid data.")
+            },
+            Error::ExternalAPIError(err) => {
+                write!(f, "Cannot execute: {}", err)
+            },
+            Error::ClientError(err) => {
+                write!(f, "External Client error: {}", err)
+            },
+            Error::ServerError(err) => {
+                write!(f, "External Server error: {}", err)
             }
         }
     }
 }
 
 impl Reject for Error {}
+impl Reject for APILayerError {}
