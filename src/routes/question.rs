33d62946ddd1da656a729a87e95ace279af062f4
SivaKrishna <shivataraya@gmail.com>
2022-12-28 17:43:24 +0530

    execute futures concurrently

--- src/routes/question.rs
+++ src/routes/question.rs
@@ -61,20 +61,23 @@ pub async fn update_question(
     store: Store,
     question: Question,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    let title = match check_profanity(question.title).await {
-        Ok(res) => res,
-        Err(e) => return Err(warp::reject::custom(e)),
-    };
+    let title = check_profanity(question.title);
+    let content = check_profanity(question.content);
 
-    let content = match check_profanity(question.content).await {
-        Ok(res) => res,
-        Err(e) => return Err(warp::reject::custom(e)),
-    };
+    let (title, content) = tokio::join!(title, content);
+
+    if title.is_err() {
+        return Err(warp::reject::custom(title.unwrap_err()));
+    }
+
+    if content.is_err() {
+        return Err(warp::reject::custom(content.unwrap_err()));
+    }
 
     let question = Question {
         id: question.id,
-        title,
-        content,
+        title: title.unwrap(),
+        content: content.unwrap(),
         tags: question.tags,
     };
 
