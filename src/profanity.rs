5f6601098c2d3c40a6acb8cd74b012c5660fe05e
SivaKrishna <shivataraya@gmail.com>
2022-12-28 17:07:37 +0530

    add bad words api for profanity check

---
+++ src/profanity.rs
@@ -0,0 +1,57 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct APIResponse {
+    message: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct BadWord {
+    original: String,
+    word: String,
+    deviations: i64,
+    info: i64,
+    #[serde(rename = "replacedLen")]
+    replaced_len: i64,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct BadWordsResponse {
+    content: String,
+    bad_words_total: i64,
+    bad_words_list: Vec<BadWord>,
+    censored_content: String,
+}
+
+pub async fn check_profanity(content: String) -> Result<String, handle_errors::Error> {
+    let client = reqwest::Client::new();
+    let res = client
+        .post("https://api.apilayer.com/bad_words?censor_character=*")
+        .header("apikey", "dummy")
+        .body(content)
+        .send()
+        .await
+        .map_err(handle_errors::Error::ExternalAPIError)?;
+
+    if !res.status().is_success() {
+        if res.status().is_client_error() {
+            let err = transform_error(res).await;
+            return Err(handle_errors::Error::ClientError(err));
+        } else {
+            let err = transform_error(res).await;
+            return Err(handle_errors::Error::ServerError(err));
+        }
+    }
+
+    match res.json::<BadWordsResponse>().await {
+        Ok(res) => Ok(res.censored_content),
+        Err(e) => Err(handle_errors::Error::ExternalAPIError(e)),
+    }
+}
+
+async fn transform_error(res: reqwest::Response) -> handle_errors::APILayerError {
+    handle_errors::APILayerError {
+        status: res.status().as_u16(),
+        message: res.json::<APIResponse>().await.unwrap().message,
+    }
+}
--- src/routes/question.rs
+++ src/routes/question.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use tracing::{event, instrument, Level};
 use warp::{http::StatusCode, Rejection, Reply};
 
+use crate::profanity::check_profanity;
 use crate::store::Store;
 use crate::types::pagination::Pagination;
 use crate::types::question::NewQuestion;
@@ -33,10 +34,26 @@ pub async fn add_question(
     store: Store,
     new_question: NewQuestion,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    if let Err(e) = store.add_question(new_question).await {
-        return Err(warp::reject::custom(e));
+    let title = match check_profanity(new_question.title).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
+
+    let content = match check_profanity(new_question.content).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
+
+    let question = NewQuestion {
+        title,
+        content,
+        tags: new_question.tags,
+    };
+
+    match store.add_question(question).await {
+        Ok(question) => Ok(warp::reply::json(&question)),
+        Err(e) => Err(warp::reject::custom(e)),
     }
-    Ok(warp::reply::with_status("Question added", StatusCode::OK))
 }
 
 pub async fn update_question(
@@ -44,12 +61,27 @@ pub async fn update_question(
     store: Store,
     question: Question,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    let res = match store.update_question(question, id).await {
+    let title = match check_profanity(question.title).await {
         Ok(res) => res,
         Err(e) => return Err(warp::reject::custom(e)),
     };
 
-    Ok(warp::reply::json(&res))
+    let content = match check_profanity(question.content).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
+
+    let question = Question {
+        id: question.id,
+        title,
+        content,
+        tags: question.tags,
+    };
+
+    match store.update_question(question, id).await {
+        Ok(res) => Ok(warp::reply::json(&res)),
+        Err(e) => Err(warp::reject::custom(e)),
+    }
 }
 
 pub async fn delete_question(id: i32, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
