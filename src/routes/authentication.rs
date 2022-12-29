d73c8b223dafb4760b814a0f21f35fd7ecde77a7
SivaKrishna <shivataraya@gmail.com>
2022-12-29 18:25:35 +0530

    hash pasword

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -1,11 +1,26 @@
+use argon2::{self, Config};
+use rand::Rng;
 use warp::http::StatusCode;
 
 use crate::store::Store;
 use crate::types::account::Account;
 
 pub async fn register(store: Store, account: Account) -> Result<impl warp::Reply, warp::Rejection> {
+    let hashed_password = hash_password(account.password.as_bytes());
+
+    let account = Account {
+        id: account.id,
+        email: account.email,
+        password: hashed_password,
+    };
     match store.add_account(account).await {
         Ok(_) => Ok(warp::reply::with_status("Account added", StatusCode::OK)),
         Err(e) => Err(warp::reject::custom(e)),
     }
+}
+
+pub fn hash_password(password: &[u8]) -> String {
+    let salt = rand::thread_rng().gen::<[u8; 32]>();
+    let config = Config::default();
+    argon2::hash_encoded(password, &salt, &config).unwrap()
 }
