edb737270e9fe38a77c6c5d9aabb828f38e8b501
SivaKrishna <shivataraya@gmail.com>
2023-01-01 00:00:01 +0530

    add integration tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all)]
 
 pub use handle_errors;
+use tokio::sync::{oneshot, oneshot::Sender};
 use tracing_subscriber::fmt::format::FmtSpan;
 use warp::{http::Method, Filter};
 
@@ -9,6 +10,10 @@ mod profanity;
 mod routes;
 mod store;
 pub mod types;
+
+pub struct OneshotHandler {
+    pub sender: Sender<i32>,
+}
 
 async fn build_routes(store: store::Store) -> impl Filter<Extract = impl warp::Reply> + Clone {
     let store_filter = warp::any().map(move || store.clone());
@@ -125,4 +130,22 @@ pub async fn setup_store(config: &config::Config) -> Result<store::Store, handle
 pub async fn run(config: config::Config, store: store::Store) {
     let routes = build_routes(store).await;
     warp::serve(routes).run(([0, 0, 0, 0], config.port)).await;
+}
+
+pub async fn oneshot(store: store::Store) -> OneshotHandler {
+    let routes = build_routes(store).await;
+    let (tx, rx) = oneshot::channel::<i32>();
+
+    let socket: std::net::SocketAddr = "127.0.0.1:3030"
+        .to_string()
+        .parse()
+        .expect("Not a valid address");
+
+    let (_, server) = warp::serve(routes).bind_with_graceful_shutdown(socket, async {
+        rx.await.ok();
+    });
+
+    tokio::task::spawn(server);
+
+    OneshotHandler { sender: tx }
 }
